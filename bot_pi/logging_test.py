d6ace7b185eb49e35bd6d98aa96c8aa4c6aaacb0
catexis <anderson.perm@gmail.com>
2018-04-01 18:35:33 +0500

    Ready for logging

--- bot_pi/logging_test.py
+++ bot_pi/logging_test.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 import sys, os
 import django
-import logging
-sys.path.append(".")
+
+sys.path.append(os.path.join(sys.path[0], ".."))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pi_web.settings")
 django.setup()
-logging.basicConfig(level=logging.DEBUG)
+
+from dashboard import models
+
+# log_string = models.LogBotPi.objects.create(history = 'Hello World!')
+# log_string.save()
+
+print(
+    models.LogBotPi.objects.all().first().time_stamp.strftime("%Y-%m-%d %H:%M"),
+    models.LogBotPi.objects.all().first().history
+)
+
